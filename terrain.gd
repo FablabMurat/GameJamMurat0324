71537746ad1378ce64efaf79f9340493c5b581a9
Flat <flat@gmx.com>
2024-03-02 12:17:34 +0100

    Préparation de la création des rochers

--- terrain.gd
+++ terrain.gd
@@ -1,21 +1,26 @@
 extends Node3D
 
 const NBSAPINS = 100
+const NBBUCHES = 40
+const NBROCHERS = 80
 const SapinScene = preload("res://sapin.tscn")
+const BucheScene = preload("res://buche.tscn")
+const RocherScene = preload("res://buche.tscn")
 
 var camrot = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	createsapins()
+	createbuches()
 	pass # Replace with function body.
 
 func createsapins():
 	var x: int
 	var z: int
 	for i in range(NBSAPINS):
-		x = randi_range(-99,99)
-		z = randi_range(-99,99)
+		x = randf()*198-99
+		z = randf()*198-99
 		createsapin(x,z)
 		
 func createsapin(x,z):
@@ -25,6 +30,40 @@ func createsapin(x,z):
 	obs.position.x = x
 	obs.position.z = z
 	add_child(obs)
+
+
+func createbuches():
+	var x: int
+	var z: int
+	for i in range(NBBUCHES):
+		x = randf()*198-99
+		z = randf()*198-99
+		createbuche(x,z)
+		
+func createbuche(x,z):
+	if abs(x) < 3 and abs(z) < 3:
+		return
+	var buche = BucheScene.instantiate()
+	buche.position.x = x
+	buche.position.z = z
+	add_child(buche)
+
+
+func createrochers():
+	var x: int
+	var z: int
+	for i in range(NBROCHERS):
+		x = randf()*198-99
+		z = randf()*198-99
+		createbuche(x,z)
+		
+func createrocher(x,z):
+	if abs(x) < 3 and abs(z) < 3:
+		return
+	var buche = RocherScene.instantiate()
+	buche.position.x = x
+	buche.position.z = z
+	add_child(buche)
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
