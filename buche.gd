fd17f220aa9e06a80fe2c2d6d579e7e6b8a3e2c5
Flat <flat@gmx.com>
2024-03-02 17:20:34 +0100

    Correction (moche) bug double collecte des buches

--- buche.gd
+++ buche.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 signal buchecollectedbyplayer
 
+var supprimee = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
