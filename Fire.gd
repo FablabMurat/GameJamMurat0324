ae4d7d63eda20c643685d41080a63f098fd443b9
Flat <flat@gmx.com>
2024-03-02 12:34:00 +0100

    Problème de conflit Préparation ajout rochers

--- Fire.gd
+++ Fire.gd
@@ -4,15 +4,11 @@ var fireSize = 0.5
 
 
 func _process(delta):
-	print($Timer.time_left)
+	#print($Timer.time_left)
 	var timeLeft = $Timer.time_left
-	if Input.is_action_just_pressed("addLog"):
-		
-		$Timer.start(timeLeft + 2.0)
+	#if Input.is_action_just_pressed("addLog"):
+	#	$Timer.start(timeLeft + 2.0)
 	$GPUParticles3D.lifetime = timeLeft/4
-
-
-
 
 func _on_timer_timeout():
 	pass # Replace with function body.
