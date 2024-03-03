1549947da263e577d078ee74a493a8120b620cf1
Ninjdai <ninjdai@duck.com>
2024-03-03 14:14:25 +0100

    Ajout menu démarrage

--- overlay.gd
+++ overlay.gd
@@ -7,14 +7,29 @@ func _ready():
 	$PauseMenu/PauseButton.pressed.connect(toggle_pause)
 	$PauseMenu/QuitButton.pressed.connect(quit_game)
 	$PauseMenu/RestartButton.pressed.connect(restart_game)
+	$PauseMenu/StartButton.pressed.connect(start_game)
+	
 	$PauseMenu/RestartButton.visible = false
+	$PauseMenu/PauseButton.visible = false
 	$PauseMenu/GameOver.visible = false
+	$PauseMenu/StartButton.visible = true
+	get_tree().paused = true
+	
 	init_displays()
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("pause_toggle"):
 		toggle_pause()
+
+func start_game():
+	get_tree().paused = false
+	$PauseMenu.visible = false
+	$PauseMenu/PauseButton.visible = true
+	$PauseMenu/StartButton.visible = false
+	
+	
+	
 
 func toggle_pause():
 	get_tree().paused = not get_tree().paused
