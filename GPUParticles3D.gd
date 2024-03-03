95ffc93c5922adee3169c8bc346e8179291a2313
Ninjdai <ninjdai@duck.com>
2024-03-03 14:42:05 +0100

    Ajout texture pas

--- GPUParticles3D.gd
+++ GPUParticles3D.gd
@@ -1,3 +1,3 @@
 extends GPUParticles3D
-draw_pass_1.material.alberto_texture
- animated_texture.frame = randi() % animated_texture.frame
+#draw_pass_1.material.alberto_texture
+#animated_texture.frame = randi() % animated_texture.frame
