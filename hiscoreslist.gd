308672db438483b0f18c81807c7c03f7598a4f01
Flat <flatweb4ali@gmx.com>
2024-03-11 15:32:31 +0100

    Top 20 sur 2 colonnes, avec highlight du 1er et du joueur

--- hiscoreslist.gd
+++ hiscoreslist.gd
@@ -1,5 +1,6 @@
 extends PanelContainer
 
+const MAX_LINES = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,51 +14,104 @@ func resized():
 	self.size = vp.size
 	self.size = vp.size
 
-func display(scores, player = ""):
-	var first = true
-	var firstplayer = true
-	for sc in scores:
-		var nom = Label.new()
-		nom.text = sc["player"]
-		if firstplayer and player == sc["player"]:
-			# Highlight nom
-			#nom.add_theme_font_override("bold",font)
-			nom.add_theme_color_override("font_color", Color(255, 0, 0))
-			pass
-		%TabContainer.add_child(nom)
-		var val : Label = Label.new()
-		val.text = "%d" % sc["pts"]
-		val.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		if firstplayer and player == sc["player"]:
-			# Highlight nom
-			val.add_theme_color_override("font_color", Color(255, 0, 0))
-			pass
-		#val.theme_override_font_sizes.font_size = 30
-		#val.add_theme_color_override("font_color", Color(255, 0, 0))
-		val.add_theme_font_size_override("font_size",25)
-		%TabContainer.add_child(val)
-		var date = Label.new()
-		date.text = sc["date"]
-		date.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
-		if firstplayer and player == sc["player"]:
-			# Highlight nom
-			date.add_theme_color_override("font_color", Color(255, 0, 0))
-			pass
-		%TabContainer.add_child(date)
-		
-		if firstplayer and player == sc["player"]:
-			firstplayer = false
-		
-		if first:
-			%TabContainer.add_child(HSeparator.new())
-			%TabContainer.add_child(HSeparator.new())
-			%TabContainer.add_child(HSeparator.new())
-		first = false
+func display(scores : Array, player = ""):
+	var sz = scores.size()
+	if sz == 0:
+		return
+	var me : bool = true
+	var needvseparator = false  # ajouter un VSeparator avant ?
+	for i in range(0,1+(sz-1)/MAX_LINES):
+		if needvseparator:
+			%HBox.add_child(VSeparator.new())
+		var grid : GridContainer
+		grid = newgrid()
+		%HBox.add_child(grid)
+		displayheader(grid)
+		for isc in range(i*MAX_LINES, min(sz,(i+1)*MAX_LINES)):
+			var highlightme = false
+			if (me and player == scores[isc]["player"]) :
+				me = false
+				highlightme = true
+			displayone(grid, isc+1, scores[isc], highlightme)
+		needvseparator = true
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func newgrid():
+	var grid : GridContainer
+	grid = GridContainer.new()
+	grid.columns = 4
+	return grid
 
+func displayheader(grid : GridContainer):
+	const HEADER_COLOR = Color(30, 80, 80)
+	var label : Label
+	label = Label.new()
+	label.text = "#"
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.add_theme_color_override("font_color", HEADER_COLOR)
+			
+	grid.add_child(label)
+	
+	label = Label.new()
+	label.text = "Joueur"
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.add_theme_color_override("font_color", HEADER_COLOR)
+	grid.add_child(label)
+
+	label = Label.new()
+	label.text = "Score"
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.add_theme_color_override("font_color", HEADER_COLOR)
+	grid.add_child(label)
+
+	label = Label.new()
+	label.text = "Date"
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.add_theme_color_override("font_color", HEADER_COLOR)
+	grid.add_child(label)
+	
+	for i in grid.columns:
+		grid.add_child(HSeparator.new())
+		
+func highlight(label : Label):
+	label.add_theme_color_override("font_color", Color(255, 0, 0))
+	
+func highlightfirst(label : Label):
+	label.add_theme_color_override("font_color", Color(0, 255, 0))
+	
+func displayone(grid : Container, rang : int, sc, \
+				highlightme : bool):
+	# Position dans le classement
+	var pos = Label.new()
+	pos.text = "%d" % rang
+	if rang == 1 : highlightfirst(pos)
+	if highlightme: highlight(pos)
+	pos.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+	grid.add_child(pos)
+	
+	# Nom du joueur
+	var nom = Label.new()
+	nom.text = sc["player"]
+	if rang == 1 : highlightfirst(nom)
+	if highlightme: highlight(nom)
+	nom.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	grid.add_child(nom)
+	
+	# Nombre de points
+	var val : Label = Label.new()
+	val.text = "%d" % sc["pts"]
+	val.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	if rang == 1 : highlightfirst(val)
+	if highlightme: highlight(val)
+	val.add_theme_font_size_override("font_size",25)
+	grid.add_child(val)
+	
+	# Date du score enregistré
+	var date = Label.new()
+	date.text = sc["date"]
+	date.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+	if rang == 1 : highlightfirst(date)
+	if highlightme: highlight(date)
+	grid.add_child(date)
 
 func _on_fermer_pressed():
 	self.queue_free()
